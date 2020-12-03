edb04e60817586e4cbcecb4f0edfc4c0ac5063ea
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-12-03 10:06:00 +0800

    Terraform

---
+++ 5.Terraform/instance/main.tf
@@ -0,0 +1,23 @@
+variable "instance_name" {}
+variable "instance_zone" {}
+variable "instance_type" {
+  default = "n1-standard-1"
+}
+variable "instance_network" {}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "${var.instance_name}"
+  zone         = "${var.instance_zone}"
+  machine_type = "${var.instance_type}"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "${var.instance_network}"
+    access_config {
+      # Allocate a one-to-one NAT IP to the instance
+    }
+  }
+}
---
+++ 5.Terraform/mynetwork.tf
@@ -0,0 +1,32 @@
+resource "google_compute_network" "mynetwork" {
+  name                    = "mynetwork"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+  name = "mynetwork-allow-http-ssh-rdp-icmp"
+  network = google_compute_network.mynetwork.self_link
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+}
+
+# Create the mynet-us-vm instance
+module "mynet-us-vm" {
+  source           = "instance"
+  instance_name    = "mynet-us-vm"
+  instance_zone    = "us-central1-a"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+
+# Create the mynet-eu-vm" instance
+module "mynet-eu-vm" {
+  source           = "instance"
+  instance_name    = "mynet-eu-vm"
+  instance_zone    = "europe-west1-d"
+  instance_network = google_compute_network.mynetwork.self_link
+}
