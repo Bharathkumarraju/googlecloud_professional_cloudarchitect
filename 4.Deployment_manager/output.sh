7a32aa94a6472947d9ffa0e57b9014caa294110b
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-12-03 09:20:46 +0800

    Deployment manager in google cloud

---
+++ 4.Deployment_manager/output.sh
@@ -0,0 +1,12 @@
+student_03_83164088d1b5@cloudshell:~ (qwiklabs-gcp-03-0ae16056878b)$ gcloud deployment-manager deployments create dminfra --config=config.yaml --preview
+The fingerprint of the deployment is b'bT05fjmh1wGW9YbQNyKGoA=='
+Waiting for create [operation-1606958266252-5b58520a0fd2c-c2e37fd8-b8a4a9f4]...done.
+Create operation operation-1606958266252-5b58520a0fd2c-c2e37fd8-b8a4a9f4 completed successfully.
+NAME                               TYPE                 STATE       ERRORS  INTENT
+mynet-eu-vm                        compute.v1.instance  IN_PREVIEW  []      CREATE_OR_ACQUIRE
+mynet-us-vm                        compute.v1.instance  IN_PREVIEW  []      CREATE_OR_ACQUIRE
+mynetwork                          compute.v1.network   IN_PREVIEW  []      CREATE_OR_ACQUIRE
+mynetwork-allow-http-ssh-rdp-icmp  compute.v1.firewall  IN_PREVIEW  []      CREATE_OR_ACQUIRE
+student_03_83164088d1b5@cloudshell:~ (qwiklabs-gcp-03-0ae16056878b)$
+
+
