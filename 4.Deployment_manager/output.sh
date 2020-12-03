150ffd834fd680827f9fecdf714f2fef3fd8ad74
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-12-03 09:28:59 +0800

    Deployment manager

--- 4.Deployment_manager/output.sh
+++ 4.Deployment_manager/output.sh
@@ -9,4 +9,19 @@ mynetwork                          compute.v1.network   IN_PREVIEW  []      CREA
 mynetwork-allow-http-ssh-rdp-icmp  compute.v1.firewall  IN_PREVIEW  []      CREATE_OR_ACQUIRE
 student_03_83164088d1b5@cloudshell:~ (qwiklabs-gcp-03-0ae16056878b)$
 
+student_03_83164088d1b5@cloudshell:~ (qwiklabs-gcp-03-0ae16056878b)$ gcloud deployment-manager deployments update dminfra
+The fingerprint of the deployment is b'nHrNsQP2oZxlZFYnZyw2nA=='
+Waiting for update [operation-1606958503913-5b5852ecb66eb-f4fd2888-15ec3047]...done.
+Update operation operation-1606958503913-5b5852ecb66eb-f4fd2888-15ec3047 completed successfully.
+NAME                               TYPE                 STATE      ERRORS  INTENT
+mynet-eu-vm                        compute.v1.instance  COMPLETED  []
+mynet-us-vm                        compute.v1.instance  COMPLETED  []
+mynetwork                          compute.v1.network   COMPLETED  []
+mynetwork-allow-http-ssh-rdp-icmp  compute.v1.firewall  COMPLETED  []
+student_03_83164088d1b5@cloudshell:~ (qwiklabs-gcp-03-0ae16056878b)$
 
+
+
+Without --preview and update directly we can run as below.
+
+gcloud deployment-manager deployments create dminfra --config=config.yaml
